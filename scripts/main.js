4a678280bb39baf1c30141b2b18a7df54745ea71
TrentSisson <trent.c.sisson0@gmail.com>
2020-10-18 04:35:00 -0500

    added criminalprovider.js and officersprovider.js, fetched criminal api in criminal provider and exported get criminals function, and same for officers,

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-console.log("Welcome to the main module")
+import {useOfficers, getOfficers} from "./officers/OfficerProvider.js"
+
+
+import {useCriminals} from "./criminals/CriminalsProvider.js"
+useCriminals()
+
+
+
+getOfficers()
+.then(() => {const officerArray = useOfficers}
+)
