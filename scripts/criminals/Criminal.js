89df0d626b910beb485ca6b79477abecbfc5bc5d
TrentSisson <trent.c.sisson0@gmail.com>
2020-10-23 10:11:17 -0500

    added officersSelect.js and modified slightly some syntax errors.

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -3,9 +3,9 @@ export const Criminals = (criminalObj) => {
     <div class="criminal">
         <h5>${criminalObj.name}</h5>
         <p>Age: ${criminalObj.age}</p>
-        <p>Crime; ${criminalObj.conviction}</p>
-        <p>Term start:${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
-        <p>Term end:${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
+        <p>Crime: ${criminalObj.conviction}</p>
+        <p>Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
+        <p>Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
 
     </div> 
     `
---
+++ scripts/officers/OfficerSelect.js
@@ -0,0 +1,42 @@
+import { getOfficers, useOfficers } from "./OfficerProvider.js"
+
+const officerFilterContainer = document.querySelector("filters__officer")
+const eventHub = document.querySelector(".container")
+
+export const OfficerSelect = () => {
+    getOfficers()
+    .then(() => {
+        const officerArray = useOfficers()
+        render(officerArray)
+    })
+} 
+const render = (officers) => {
+    officerFilterContainer.innerHTML = `
+    <select class= "dropedown" id="officerSelect">
+    <option value="0">Please select an officer...</option>
+    ${officers.map(
+officerObj => {
+return `<option value="${officerObj.name}">${officerObj.name}</option>`
+}
+).join("")
+}
+</select>
+`
+}
+
+eventHub.addEventListener("change", (changeEvent) => {
+if (changeEvent.target.id === "officerSelect") {
+console.log("OfficerSelect: Change event happened in the officers dropdown")
+
+console.log("OfficerSelect: Build custom event for officerSelected")
+const officerSelectedEvent = new CustomEvent("officerSelected", {
+detail: {
+officerName: changeEvent.target.value
+}
+})
+
+console.log("OfficerSelect: Dispatch officerSelected event to event hub")
+eventHub.dispatchEvent(officerSelectedEvent)
+}
+})
+
